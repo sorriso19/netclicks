5bcf7681071b60f10270a1b82725063cc8c2db84
sorriso19 <sorriso19@mail.ru>
2024-10-04 01:59:57 +0300

    Angularproject

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchComponent } from './components/search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/constants/films.constants.ts
@@ -0,0 +1,32 @@
+import { Film } from "../models/film.model";
+
+export const FILMS: Film[] = [
+
+    {
+        id: 0,
+        title: 'Марсианин 1',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './img/film.png'
+    },
+    {
+        id: 1,
+        title: 'Марсианин 2',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './img/film.png'
+    },
+    {
+        id: 2,
+        title: 'Марсианин 3',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: './film.html',
+        image: './img/film.png'
+    },
+]
